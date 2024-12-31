6b641bdfc4034435a1564544d0bcb4e1df08bb2d
mk-isos <mkisos7900@naver.com>
2024-12-31 16:06:54 +0900

    241231 Create chap1 sec03

--- chap1/src/sec03/exam01/Hello.java
+++ chap1/src/sec03/exam01/Hello.java
@@ -1,7 +1,13 @@
 package sec03.exam01;
 
+/*
+주석 사용 예시
+*/
+
 public class Hello {
+    // 프로그램 실행 진입점
     public static void main(String[] args) {
+        //콘솔에 출력하느 실행문
         System.out.println("Hello, Java");
-    }
-}
+    } // end of main
+} // end of class
